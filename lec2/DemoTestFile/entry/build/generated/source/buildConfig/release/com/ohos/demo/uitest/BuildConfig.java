0f88ce3d2f568283c53b970541e8929abd5916d4
cascades-sjtu <cascades@sjtu.edu.cn>
2021-05-22 19:23:16 +0800

    update

---
+++ lec2/DemoTestFile/entry/build/generated/source/buildConfig/release/com/ohos/demo/uitest/BuildConfig.java
@@ -0,0 +1,11 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ohos.demo.uitest;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = false;
+    public static final String PACKAGE_NAME = "com.ohos.demo.uitest";
+    public static final String BUILD_TYPE = "release";
+    public static final int COMPILE_SDK_VERSION = 4;
+}
